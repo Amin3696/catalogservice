d335945857fb1dfbb0482f871ffac0e7afd68f39
Amin Shahimian <mohammadamin.shahimian@sti-consulting.com>
2023-05-09 15:24:45 +0200

    add postgres db dependency & config & docker-compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,8 @@ dependencies {
     //logging
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 
-    runtimeOnly("com.h2database:h2")
-    //runtimeOnly("org.postgresql:postgresql")
+    //runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
